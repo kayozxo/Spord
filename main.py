06c65611b7e7e45599634663c1c23b0f78ad6955
Manoj <penugurthimanoj@gmail.com>
2024-03-06 21:36:16 +0530

    registration works with tkinter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import json, hashlib, getpass, os, sys
 from cryptography.fernet import Fernet
+import tkinter as tk
+import customtkinter as ctk
+from CTkMessagebox import CTkMessagebox
 
 def hash_password(password):
    sha256 = hashlib.sha256()
@@ -18,18 +21,25 @@ def encrypt_password(cipher, password):
 def decrypt_password(cipher, encrypted_password):
    return cipher.decrypt(encrypted_password.encode()).decode()
 
-def register(username, master_password):
+def register():
+   file = 'user_data.json'
+   if os.path.exists(file) and os.path.getsize(file) != 0:
+           CTkMessagebox(title="Info", message="Master user already exists!!")
+           sys.exit()
+   else:
+           username = register_username.get()
+           master_password = register_master_password.get()
    hashed_master_password = hash_password(master_password)
    user_data = {'username': username, 'master_password': hashed_master_password}
    file_name = 'user_data.json'
    if os.path.exists(file_name) and os.path.getsize(file_name) == 0:
        with open(file_name, 'w') as file:
            json.dump(user_data, file)
-           print("\n[+] Registration complete!!\n")
+           CTkMessagebox(title="Info", message="Registration complete!!")
    else:
        with open(file_name, 'x') as file:
            json.dump(user_data, file)
-           print("\n[+] Registration complete!!\n")
+           CTkMessagebox(title="Info", message="Registration complete!!")
 
 def login(username, entered_password):
    try:
@@ -102,7 +112,7 @@ def get_password(website):
            return decrypted_password
    return None
 
-
+'''
 while True:
    print("1. Register")
    print("2. Login")
@@ -154,4 +164,45 @@ while True:
                break
    elif choice == '3':
        break
+'''
 
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('green')
+
+app = ctk.CTk()
+app.geometry("800x500")
+app.title("Password Manager")
+
+app.resizable(False, False)
+
+register_frame = ctk.CTkFrame(app, width=400, height=500, corner_radius=0)
+register_frame.pack(side="right", fill="both", expand=True)
+
+register_username = ctk.CTkEntry(register_frame, placeholder_text="Username")
+register_username.pack(pady=12, padx=10)
+
+register_master_password = ctk.CTkEntry(register_frame, placeholder_text="Master Password", show="•")
+register_master_password.pack(pady=12, padx=10)
+
+register_button = ctk.CTkButton(register_frame, text="Register", command=register)
+register_button.pack(pady=12, padx=10)
+
+login_frame = ctk.CTkFrame(app, width=400, height=500, corner_radius=0)
+login_frame.pack(side="left", fill="both", expand=True)
+
+login_username = ctk.CTkEntry(login_frame, placeholder_text="Username")
+login_username.pack(pady=12, padx=10)
+
+login_master_password = ctk.CTkEntry(login_frame, placeholder_text="Master Password", show="•")
+login_master_password.pack(pady=12, padx=10)
+
+login_button = ctk.CTkButton(login_frame, text="Login", command=lambda: login(login_username.get(), login_master_password.get()))
+login_button.pack(pady=12, padx=10)
+
+main_frame = ctk.CTkFrame(app, width=400, height=500, corner_radius=0)
+main_frame.pack(side="top", fill="both", expand=True)
+
+main_label = ctk.CTkLabel(main_frame, text="Password Manager")
+main_label.pack(pady=12, padx=10)
+
+app.mainloop()

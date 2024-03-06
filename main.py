32136290191fb9074b6885bd7b0a181c2861fc68
Manoj <penugurthimanoj@gmail.com>
2024-03-06 19:47:44 +0530

    reworked entire code, works in terminal, ui is pending, and it now uses json files to store information instead of text files

--- main.py
+++ main.py
@@ -1,200 +1,157 @@
-import tkinter as tk
-from tkinter import messagebox
+import json, hashlib, getpass, os, sys
 from cryptography.fernet import Fernet
 
-class App:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Password App")
-        self.root.geometry("500x500")
+def hash_password(password):
+   sha256 = hashlib.sha256()
+   sha256.update(password.encode())
+   return sha256.hexdigest()
 
-        self.password_set = False
-        self.key = None
+def generate_key():
+   return Fernet.generate_key()
 
-        self.check_password()
+def initialize_cipher(key):
+   return Fernet(key)
 
-        self.login_frame = tk.Frame(root)
+def encrypt_password(cipher, password):
+   return cipher.encrypt(password.encode()).decode()
 
-        self.label_login = tk.Label(self.login_frame, text="Enter Password:", pady=10)
-        self.label_login.pack()
+def decrypt_password(cipher, encrypted_password):
+   return cipher.decrypt(encrypted_password.encode()).decode()
 
-        self.password_entry_login = tk.Entry(self.login_frame, show="*")
-        self.password_entry_login.pack(pady=10)
+def register(username, master_password):
+   hashed_master_password = hash_password(master_password)
+   user_data = {'username': username, 'master_password': hashed_master_password}
+   file_name = 'user_data.json'
+   if os.path.exists(file_name) and os.path.getsize(file_name) == 0:
+       with open(file_name, 'w') as file:
+           json.dump(user_data, file)
+           print("\n[+] Registration complete!!\n")
+   else:
+       with open(file_name, 'x') as file:
+           json.dump(user_data, file)
+           print("\n[+] Registration complete!!\n")
 
-        self.submit_button_login = tk.Button(self.login_frame, text="Login", command=self.check_login)
-        self.submit_button_login.pack(pady=10)
+def login(username, entered_password):
+   try:
+       with open('user_data.json', 'r') as file:
+           user_data = json.load(file)
+       stored_password_hash = user_data.get('master_password')
+       entered_password_hash = hash_password(entered_password)
+       if entered_password_hash == stored_password_hash and username == user_data.get('username'):
+           print("\n[+] Login Successful..\n")
+       else:
+           print("\n[-] Invalid Login credentials. Please use the credentials you used to register.\n")
+           sys.exit()
+   except Exception:
+       print("\n[-] You have not registered. Please do that.\n")
+       sys.exit()
 
-        self.password_setup_frame = tk.Frame(root)
+def view_websites():
+   try:
+       with open('passwords.json', 'r') as data:
+           view = json.load(data)
+           print("\nWebsites you saved...\n")
+           for x in view:
+               print(x['website'])
+           print('\n')
+   except FileNotFoundError:
+       print("\n[-] You have not saved any passwords!\n")
 
-        self.label_setup = tk.Label(self.password_setup_frame, text="Set Password:", pady=10)
-        self.label_setup.pack()
+key_filename = 'encryption_key.key'
+if os.path.exists(key_filename):
+   with open(key_filename, 'rb') as key_file:
+       key = key_file.read()
+else:
+   key = generate_key()
+   with open(key_filename, 'wb') as key_file:
+       key_file.write(key)
 
-        self.password_entry_setup = tk.Entry(self.password_setup_frame, show="*")
-        self.password_entry_setup.pack(pady=10)
+cipher = initialize_cipher(key)
 
-        self.submit_button_setup = tk.Button(self.password_setup_frame, text="Submit", command=self.save_password)
-        self.submit_button_setup.pack(pady=10)
+def add_password(website, password):
+   if not os.path.exists('passwords.json'):
+       data = []
+   else:
+       try:
+           with open('passwords.json', 'r') as file:
+               data = json.load(file)
+       except json.JSONDecodeError:
+           data = []
+   encrypted_password = encrypt_password(cipher, password)
 
-        self.main_frame = tk.Frame(root)
+   password_entry = {'website': website, 'password': encrypted_password}
+   data.append(password_entry)
 
-        self.label_main = tk.Label(self.main_frame, text="Welcome to the App!", font=('Helvetica', 12), pady=20)
-        self.label_main.pack()
+   with open('passwords.json', 'w') as file:
+       json.dump(data, file, indent=4)
 
-        self.button_main = tk.Button(self.main_frame, text="Logout", command=self.logout)
-        self.button_main.pack(pady=10)
+def get_password(website):
 
-        instructions = '''To add password fill all the fields and press "Add Password"
-        To view password, enter Account Name and press "Get Password"'''
+   if not os.path.exists('passwords.json'):
+       return None
 
-        self.instruction_label = tk.Label(self.main_frame,  text=instructions, bg="#d3d3d3")
-        self.instruction_label.pack(padx=10, pady=5)
+   try:
+       with open('passwords.json', 'r') as file:
+           data = json.load(file)
+   except json.JSONDecodeError:
+       data = []
 
-        self.service_label = tk.Label(self.main_frame, text="Account:", bg="#d3d3d3")
-        self.service_label.pack(padx=10, pady=5)
-        self.service_entry = tk.Entry(self.main_frame)
-        self.service_entry.pack(padx=10, pady=5)
-
-        self.username_label = tk.Label(self.main_frame, text="Username:", bg="#d3d3d3")
-        self.username_label.pack(padx=10, pady=5)
-        self.username_entry = tk.Entry(self.main_frame)
-        self.username_entry.pack(padx=10, pady=5)
-
-        self.password_label = tk.Label(self.main_frame, text="Password:", bg="#d3d3d3")
-        self.password_label.pack(padx=10, pady=5)
-        self.password_entry = tk.Entry(self.main_frame, show="*")
-        self.password_entry.pack(padx=10, pady=5)
+   for entry in data:
+       if entry['website'] == website:
+           decrypted_password = decrypt_password(cipher, entry['password'])
+           return decrypted_password
+   return None
 
 
-        self.add_button = tk.Button(self.main_frame, text="Add Password", command=self.add_password, height=1, width=10)
-        self.add_button.pack(padx=10, pady=5)
+while True:
+   print("1. Register")
+   print("2. Login")
+   print("3. Quit")
+   choice = input("Enter your choice: ")
+   if choice == '1':
+       file = 'user_data.json'
+       if os.path.exists(file) and os.path.getsize(file) != 0:
+           print("\n[-] Master user already exists!!")
+           sys.exit()
+       else:
+           username = input("Enter your username: ")
+           master_password = getpass.getpass("Enter your master password: ")
+           register(username, master_password)
+   elif choice == '2':
+       file = 'user_data.json'
+       if os.path.exists(file):
+           username = input("Enter your username: ")
+           master_password = getpass.getpass("Enter your master password: ")
+           login(username, master_password)
+       else:
+           print("\n[-] You have not registered. Please do that.\n")
+           sys.exit()
 
-        self.get_button = tk.Button(self.main_frame, text="Get Password", command=self.get_password, height=1, width=10)
-        self.get_button.pack(padx=10, pady=5)
+       while True:
+           print("1. Add Password")
+           print("2. Get Password")
+           print("3. View Saved websites")
+           print("4. Quit")
+           password_choice = input("Enter your choice: ")
+           if password_choice == '1':
+               website = input("Enter website: ")
+               password = getpass.getpass("Enter password: ")
 
-        if self.password_set:
-            self.show_frame(self.login_frame)
-        else:
-            self.show_frame(self.password_setup_frame)
+               add_password(website, password)
+               print("\n[+] Password added!\n")
+           elif password_choice == '2':
+               website = input("Enter website: ")
+               decrypted_password = get_password(website)
+               if website and decrypted_password:
 
-    def generate_master_key(self):
-        return Fernet.generate_key()
+                   print(f"\n[+] Password for {website}: {decrypted_password}\n[+] Password copied to clipboard.\n")
+               else:
+                   print("\n[-] Password not found! Did you save the password?"
+                         "\n[-] Use option 3 to see the websites you saved.\n")
+           elif password_choice == '3':
+               view_websites()
+           elif password_choice == '4':
+               break
+   elif choice == '3':
+       break
 
-    def generate_key(self):
-        self.key = Fernet.generate_key()
-        return self.key
-
-    def encrypt_password(self, password):
-        f = Fernet(self.key)
-        return f.encrypt(password.encode()).decode()
-
-    def decrypt_password(self, encrypted_password):
-        f = Fernet(self.key)
-        return f.decrypt(encrypted_password.encode()).decode()
-
-    def add_password(self):
-        self.key = self.generate_key()
-
-        with open("key.key", "wb") as key_file:
-            key_file.write(self.key)
-
-        service = self.service_entry.get()
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-
-        if service and username and password:
-            encrypted_password = self.encrypt_password(password)
-            enc_pwd = encrypted_password
-            with open("password.txt", "a") as pwd_txt:
-                pwd_txt.write(service + " | " + username + " | " + str(enc_pwd) + "\n")
-
-            messagebox.showinfo("Success", "Password added successfully!")
-        else:
-            messagebox.showwarning("Error", "Please fill in all the fields.")
-
-    def get_password(self):
-        service = self.service_entry.get()
-        with open("key.key", "rb") as key_file:
-            self.key = key_file.read()
-
-        with open("password.txt", "r") as get_pwd:
-            key = Fernet(self.key)
-            for line in get_pwd.readlines():
-                data = line.rstrip()
-                acc, usrn, pwd = data.split("|")
-
-                decrypted_password = self.decrypt_password(pwd)
-                if service == acc:
-                    messagebox.showinfo("Password", f"Username: {usrn}\nPassword: {decrypted_password}")
-                else:
-                    messagebox.showwarning("Error", "Password not found.")
-
-    def check_password(self):
-        try:
-            with open("master_key.key", "rb") as master_key_file:
-                self.key = master_key_file.read()
-
-            with open("master_password.txt", "rb") as file:
-                cipher_suite = Fernet(self.key)
-                decrypted_password = cipher_suite.decrypt(file.read())
-                self.password_set = bool(decrypted_password)
-        except (FileNotFoundError, ValueError):
-            self.password_set = False
-
-    def encrypt_master_password(self, password):
-        cipher_suite = Fernet(self.key)
-        encrypted_password = cipher_suite.encrypt(password.encode())
-        return encrypted_password
-
-    def show_frame(self, frame):
-        # Hide all frames
-        self.login_frame.pack_forget()
-        self.password_setup_frame.pack_forget()
-        self.main_frame.pack_forget()
-
-        # Show the specified frame
-        frame.pack()
-
-    def check_login(self):
-        entered_password = self.password_entry_login.get()
-
-        try:
-            with open("master_password.txt", "rb") as file:
-                cipher_suite = Fernet(self.key)
-                decrypted_password = cipher_suite.decrypt(file.read()).decode()
-
-            if entered_password == decrypted_password:
-                # Password is correct, show the main frame
-                self.show_frame(self.main_frame)
-            else:
-                messagebox.showerror("Error", "Invalid password. Please try again.")
-        except FileNotFoundError:
-            messagebox.showerror("Error", "Password not set. Set up a password first.")
-
-    def save_password(self):
-        self.key = self.generate_master_key()
-
-        with open("master_key.key", "wb") as master_key_file:
-            master_key_file.write(self.key)
-
-        password = self.password_entry_setup.get()
-        encrypted_password = self.encrypt_master_password(password)
-
-        with open("master_password.txt", "wb") as file:
-            file.write(encrypted_password)
-
-        messagebox.showinfo("Success", "Password set successfully!")
-
-        # Show the login frame after setting up the password
-        self.show_frame(self.login_frame)
-
-    def logout(self):
-        # Clear the password entry field
-        self.password_entry_login.delete(0, 'end')
-
-        # Show the login frame after logout
-        self.show_frame(self.login_frame)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()

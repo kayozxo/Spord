83cea3b787905565abf9db13a00d95c1dd9bf61a
Manoj <penugurthimanoj@gmail.com>
2024-03-07 05:52:17 +0530

    added ui, registration and login works with ui

--- main.py
+++ main.py
@@ -4,113 +4,170 @@ import tkinter as tk
 import customtkinter as ctk
 from CTkMessagebox import CTkMessagebox
 
-def hash_password(password):
-   sha256 = hashlib.sha256()
-   sha256.update(password.encode())
-   return sha256.hexdigest()
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry("800x500")
+        self.root.title("Password Manager")
 
-def generate_key():
-   return Fernet.generate_key()
+        self.root.resizable(False, False)
 
-def initialize_cipher(key):
-   return Fernet(key)
+        self.register_frame = ctk.CTkFrame(self.root, width=800, height=500, corner_radius=0)
+        self.register_frame.pack()
 
-def encrypt_password(cipher, password):
-   return cipher.encrypt(password.encode()).decode()
+        self.register_label = ctk.CTkLabel(self.register_frame, text="Register", font=("Roboto", 24))
+        self.register_label.pack(pady=12, padx=10)
 
-def decrypt_password(cipher, encrypted_password):
-   return cipher.decrypt(encrypted_password.encode()).decode()
+        self.register_username = ctk.CTkEntry(self.register_frame, placeholder_text="Username")
+        self.register_username.pack(pady=12, padx=10)
 
-def register():
-   file = 'user_data.json'
-   if os.path.exists(file) and os.path.getsize(file) != 0:
-           CTkMessagebox(title="Info", message="Master user already exists!!")
-           sys.exit()
-   else:
-           username = register_username.get()
-           master_password = register_master_password.get()
-   hashed_master_password = hash_password(master_password)
-   user_data = {'username': username, 'master_password': hashed_master_password}
-   file_name = 'user_data.json'
-   if os.path.exists(file_name) and os.path.getsize(file_name) == 0:
-       with open(file_name, 'w') as file:
-           json.dump(user_data, file)
-           CTkMessagebox(title="Info", message="Registration complete!!")
-   else:
-       with open(file_name, 'x') as file:
-           json.dump(user_data, file)
-           CTkMessagebox(title="Info", message="Registration complete!!")
+        self.register_master_password = ctk.CTkEntry(self.register_frame, placeholder_text="Master Password", show="•")
+        self.register_master_password.pack(pady=12, padx=10)
 
-def login(username, entered_password):
-   try:
-       with open('user_data.json', 'r') as file:
-           user_data = json.load(file)
-       stored_password_hash = user_data.get('master_password')
-       entered_password_hash = hash_password(entered_password)
-       if entered_password_hash == stored_password_hash and username == user_data.get('username'):
-           print("\n[+] Login Successful..\n")
-       else:
-           print("\n[-] Invalid Login credentials. Please use the credentials you used to register.\n")
-           sys.exit()
-   except Exception:
-       print("\n[-] You have not registered. Please do that.\n")
-       sys.exit()
+        self.register_button = ctk.CTkButton(self.register_frame, text="Register", command=self.register)
+        self.register_button.pack(pady=12, padx=10)
 
-def view_websites():
-   try:
-       with open('passwords.json', 'r') as data:
-           view = json.load(data)
-           print("\nWebsites you saved...\n")
-           for x in view:
-               print(x['website'])
-           print('\n')
-   except FileNotFoundError:
-       print("\n[-] You have not saved any passwords!\n")
+        self.login_frame = ctk.CTkFrame(self.root, width=400, height=500, corner_radius=0)
+        self.login_frame.pack(side="left", fill="both", expand=True)
 
-key_filename = 'encryption_key.key'
-if os.path.exists(key_filename):
-   with open(key_filename, 'rb') as key_file:
-       key = key_file.read()
-else:
-   key = generate_key()
-   with open(key_filename, 'wb') as key_file:
-       key_file.write(key)
+        self.login_label = ctk.CTkLabel(self.login_frame, text="Login", font=("Roboto", 24))
+        self.login_label.pack(pady=12, padx=10)
 
-cipher = initialize_cipher(key)
+        self.login_username = ctk.CTkEntry(self.login_frame, placeholder_text="Username")
+        self.login_username.pack(pady=12, padx=10)
 
-def add_password(website, password):
-   if not os.path.exists('passwords.json'):
-       data = []
-   else:
-       try:
-           with open('passwords.json', 'r') as file:
-               data = json.load(file)
-       except json.JSONDecodeError:
-           data = []
-   encrypted_password = encrypt_password(cipher, password)
+        self.login_master_password = ctk.CTkEntry(self.login_frame, placeholder_text="Master Password", show="•")
+        self.login_master_password.pack(pady=12, padx=10)
 
-   password_entry = {'website': website, 'password': encrypted_password}
-   data.append(password_entry)
+        self.login_button = ctk.CTkButton(self.login_frame, text="Login", command=self.login)
+        self.login_button.pack(pady=12, padx=10)
 
-   with open('passwords.json', 'w') as file:
-       json.dump(data, file, indent=4)
+        self.main_frame = ctk.CTkFrame(self.root, width=400, height=500, corner_radius=0)
+        self.main_frame.pack(side="top", fill="both", expand=True)
 
-def get_password(website):
+        self.main_label = ctk.CTkLabel(self.main_frame, text="Password Manager")
+        self.main_label.pack(pady=12, padx=10)
 
-   if not os.path.exists('passwords.json'):
-       return None
+        self.show_frame(self.register_frame)
 
-   try:
-       with open('passwords.json', 'r') as file:
-           data = json.load(file)
-   except json.JSONDecodeError:
-       data = []
+        if os.path.exists('user_data.json') and os.path.getsize('user_data.json') != 0:
+            self.show_frame(self.login_frame)
+        else:
+            self.show_frame(self.register_frame)
 
-   for entry in data:
-       if entry['website'] == website:
-           decrypted_password = decrypt_password(cipher, entry['password'])
-           return decrypted_password
-   return None
+
+    def hash_password(self, password):
+        sha256 = hashlib.sha256()
+        sha256.update(password.encode())
+        return sha256.hexdigest()
+
+    def generate_key(self):
+        return Fernet.generate_key()
+
+    def initialize_cipher(self, key):
+        return Fernet(key)
+
+    def encrypt_password(self, cipher, password):
+        return cipher.encrypt(password.encode()).decode()
+
+    def decrypt_password(self, cipher, encrypted_password):
+        return cipher.decrypt(encrypted_password.encode()).decode()
+
+    def show_frame(self, frame):
+        self.register_frame.pack_forget()
+        self.login_frame.pack_forget()
+        self.main_frame.pack_forget()
+
+        frame.pack()
+
+    def register(self):
+        username = self.register_username.get()
+        master_password = self.register_master_password.get()
+        hashed_master_password = self.hash_password(master_password)
+        user_data = {'username': username, 'master_password': hashed_master_password}
+        file_name = 'user_data.json'
+        if os.path.exists(file_name) and os.path.getsize(file_name) == 0:
+            with open(file_name, 'w') as file:
+                json.dump(user_data, file)
+                CTkMessagebox(title="Info", message="Registration complete!!")
+                self.show_frame(self.login_frame)
+        else:
+            with open(file_name, 'x') as file:
+                json.dump(user_data, file)
+                CTkMessagebox(title="Info", message="Registration complete!!")
+                self.show_frame(self.login_frame)
+
+    def login(self):
+        username = self.login_username.get()
+        entered_password = self.login_master_password.get()
+
+        try:
+            with open('user_data.json', 'r') as file:
+                user_data = json.load(file)
+            stored_password_hash = user_data.get('master_password')
+            entered_password_hash = self.hash_password(entered_password)
+            if entered_password_hash == stored_password_hash and username == user_data.get('username'):
+                CTkMessagebox(title="Info", message="Login successful!!")
+                self.show_frame(self.main_frame)
+            else:
+                CTkMessagebox(title="Info", message="Invalid Login Credentials!")
+        except Exception:
+            CTkMessagebox(title="Error", message="You have not registerd. Please Do That!")
+
+    def view_websites(self):
+        try:
+            with open('passwords.json', 'r') as data:
+                view = json.load(data)
+                print("\nWebsites you saved...\n")
+                for x in view:
+                    print(x['website'])
+                print('\n')
+        except FileNotFoundError:
+            print("\n[-] You have not saved any passwords!\n")
+
+        key_filename = 'encryption_key.key'
+        if os.path.exists(key_filename):
+            with open(key_filename, 'rb') as key_file:
+                key = key_file.read()
+        else:
+            key = self.generate_key()
+            with open(key_filename, 'wb') as key_file:
+                key_file.write(key)
+
+        self.cipher = self.initialize_cipher(key)
+
+    def add_password(self, website, password):
+        if not os.path.exists('passwords.json'):
+            data = []
+        else:
+            try:
+                with open('passwords.json', 'r') as file:
+                    data = json.load(file)
+            except json.JSONDecodeError:
+                data = []
+        encrypted_password = self.encrypt_password(self.cipher, password)
+
+        password_entry = {'website': website, 'password': encrypted_password}
+        data.append(password_entry)
+
+        with open('passwords.json', 'w') as file:
+            json.dump(data, file, indent=4)
+
+    def get_password(self, website):
+        if not os.path.exists('passwords.json'):
+            return None
+
+        try:
+            with open('passwords.json', 'r') as file:
+                data = json.load(file)
+        except json.JSONDecodeError:
+            data = []
+
+        for entry in data:
+            if entry['website'] == website:
+                decrypted_password = self.decrypt_password(self.cipher, entry['password'])
+                return decrypted_password
+        return None
 
 '''
 while True:
@@ -166,43 +223,7 @@ while True:
        break
 '''
 
-ctk.set_appearance_mode('dark')
-ctk.set_default_color_theme('green')
-
-app = ctk.CTk()
-app.geometry("800x500")
-app.title("Password Manager")
-
-app.resizable(False, False)
-
-register_frame = ctk.CTkFrame(app, width=400, height=500, corner_radius=0)
-register_frame.pack(side="right", fill="both", expand=True)
-
-register_username = ctk.CTkEntry(register_frame, placeholder_text="Username")
-register_username.pack(pady=12, padx=10)
-
-register_master_password = ctk.CTkEntry(register_frame, placeholder_text="Master Password", show="•")
-register_master_password.pack(pady=12, padx=10)
-
-register_button = ctk.CTkButton(register_frame, text="Register", command=register)
-register_button.pack(pady=12, padx=10)
-
-login_frame = ctk.CTkFrame(app, width=400, height=500, corner_radius=0)
-login_frame.pack(side="left", fill="both", expand=True)
-
-login_username = ctk.CTkEntry(login_frame, placeholder_text="Username")
-login_username.pack(pady=12, padx=10)
-
-login_master_password = ctk.CTkEntry(login_frame, placeholder_text="Master Password", show="•")
-login_master_password.pack(pady=12, padx=10)
-
-login_button = ctk.CTkButton(login_frame, text="Login", command=lambda: login(login_username.get(), login_master_password.get()))
-login_button.pack(pady=12, padx=10)
-
-main_frame = ctk.CTkFrame(app, width=400, height=500, corner_radius=0)
-main_frame.pack(side="top", fill="both", expand=True)
-
-main_label = ctk.CTkLabel(main_frame, text="Password Manager")
-main_label.pack(pady=12, padx=10)
-
-app.mainloop()
+if __name__ == "__main__":
+    root = ctk.CTk()
+    app = App(root)
+    root.mainloop()

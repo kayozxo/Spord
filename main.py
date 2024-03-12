9438ebcef5be8433cd68974df9ffc9d802d00622
Manoj <penugurthimanoj@gmail.com>
2024-03-12 05:38:00 +0530

    99% completedgit add .! there's a bug where only the first saved password is shown and for others it shows error, will fix that in the evening

--- main.py
+++ main.py
@@ -60,13 +60,15 @@ class App:
         self.view_button = ctk.CTkButton(self.main_frame, text="View", command=self.view_websites)
         self.view_button.pack(pady=12, padx=10)
 
+        self.get_password_button = ctk.CTkButton(self.main_frame, text="Get Password", command=self.get_password)
+        self.get_password_button.pack(pady=12, padx=10)
+
         self.show_frame(self.register_frame)
 
         if os.path.exists('user_data.json') and os.path.getsize('user_data.json') != 0:
             self.show_frame(self.login_frame)
         else:
             self.show_frame(self.register_frame)
-
 
     def hash_password(self, password):
         sha256 = hashlib.sha256()
@@ -143,17 +145,13 @@ class App:
 
         self.cipher = self.initialize_cipher(key)
 
-
     def view_websites(self):
         try:
             with open('passwords.json', 'r') as data:
                 view = json.load(data)
-                print("\nWebsites you saved...\n")
-                for x in view:
-                    print(x['website'])
-                print('\n')
+                CTkMessagebox(title="Websites", message='\n'.join(''.join(x['website'].upper()) for x in view))
         except FileNotFoundError:
-            print("\n[-] You have not saved any passwords!\n")
+            CTkMessagebox(title="Error", icon="cancel", message="You have not saved any passwords!")
 
     def add_password(self):
         website = self.website.get()
@@ -176,8 +174,13 @@ class App:
 
         CTkMessagebox(title="Success", icon="check", message="Password Added Successfully!!")
 
-    def get_password(self, website):
+    def get_password(self):
+        website = self.website.get()
         if not os.path.exists('passwords.json'):
+            return None
+
+        if website == '':
+            CTkMessagebox(title="Error", icon="cancel", message="Please enter a website!")
             return None
 
         try:
@@ -189,62 +192,11 @@ class App:
         for entry in data:
             if entry['website'] == website:
                 decrypted_password = self.decrypt_password(self.cipher, entry['password'])
-                return decrypted_password
-        return None
-
-'''
-while True:
-   print("1. Register")
-   print("2. Login")
-   print("3. Quit")
-   choice = input("Enter your choice: ")
-   if choice == '1':
-       file = 'user_data.json'
-       if os.path.exists(file) and os.path.getsize(file) != 0:
-           print("\n[-] Master user already exists!!")
-           sys.exit()
-       else:
-           username = input("Enter your username: ")
-           master_password = getpass.getpass("Enter your master password: ")
-           register(username, master_password)
-   elif choice == '2':
-       file = 'user_data.json'
-       if os.path.exists(file):
-           username = input("Enter your username: ")
-           master_password = getpass.getpass("Enter your master password: ")
-           login(username, master_password)
-       else:
-           print("\n[-] You have not registered. Please do that.\n")
-           sys.exit()
-
-       while True:
-           print("1. Add Password")
-           print("2. Get Password")
-           print("3. View Saved websites")
-           print("4. Quit")
-           password_choice = input("Enter your choice: ")
-           if password_choice == '1':
-               website = input("Enter website: ")
-               password = getpass.getpass("Enter password: ")
-
-               add_password(website, password)
-               print("\n[+] Password added!\n")
-           elif password_choice == '2':
-               website = input("Enter website: ")
-               decrypted_password = get_password(website)
-               if website and decrypted_password:
-
-                   print(f"\n[+] Password for {website}: {decrypted_password}\n[+] Password copied to clipboard.\n")
-               else:
-                   print("\n[-] Password not found! Did you save the password?"
-                         "\n[-] Use option 3 to see the websites you saved.\n")
-           elif password_choice == '3':
-               view_websites()
-           elif password_choice == '4':
-               break
-   elif choice == '3':
-       break
-'''
+                CTkMessagebox(title="Password", message=f"Password For {website} : {decrypted_password}")
+                break
+            else:
+                CTkMessagebox(title="Error", icon="cancel", message="Password Not Found!")
+                break
 
 if __name__ == "__main__":
     root = ctk.CTk()

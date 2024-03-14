f57337afebde475abea1f76722486a94687a9f64
Manoj <penugurthimanoj@gmail.com>
2024-03-14 06:04:11 +0530

    updated ui

--- main.py
+++ main.py
@@ -42,23 +42,26 @@ class App:
 
         self.main_frame = ctk.CTkFrame(root, width=800, height=500)
 
-        self.main_label = ctk.CTkLabel(self.main_frame, text="Password Manager")
-        self.main_label.pack(pady=12, padx=10)
+        self.main_label = ctk.CTkLabel(self.main_frame, text="Password Manager", font=("Montserrat Black", 24))
+        self.main_label.place(x=400, y=50, anchor="center")
 
-        self.website = ctk.CTkEntry(self.main_frame, placeholder_text="Website Name")
-        self.website.pack(pady=12, padx=10)
+        self.website = ctk.CTkEntry(self.main_frame, placeholder_text="Website Name", font=("Montserrat", 12), width=250, height=40, corner_radius=5)
+        self.website.place(x=400, y=140, anchor="center")
 
-        self.password = ctk.CTkEntry(self.main_frame, placeholder_text="Password", show="•")
-        self.password.pack(pady=12, padx=10)
+        self.password = ctk.CTkEntry(self.main_frame, placeholder_text="Enter Password", show="•", font=("Montserrat", 12), width=250, height=40, corner_radius=5)
+        self.password.place(x=400, y=190, anchor="center")
 
-        self.add_button = ctk.CTkButton(self.main_frame, text="Add", command=self.add_password)
-        self.add_button.pack(pady=12, padx=10)
+        self.add_button = ctk.CTkButton(self.main_frame, text="Add Password", command=self.add_password, font=("Montserrat Medium", 12), corner_radius=5, width=250, height=34)
+        self.add_button.place(x=400, y=260, anchor="center")
 
-        self.view_button = ctk.CTkButton(self.main_frame, text="View", command=self.view_websites)
-        self.view_button.pack(pady=12, padx=10)
+        self.view_button = ctk.CTkButton(self.main_frame, text="View Websites", command=self.view_websites, font=("Montserrat Medium", 12), corner_radius=5, width=250, height=34)
+        self.view_button.place(x=400, y=320, anchor="center")
 
-        self.get_password_button = ctk.CTkButton(self.main_frame, text="Get Password", command=self.get_password)
-        self.get_password_button.pack(pady=12, padx=10)
+        self.get_password_button = ctk.CTkButton(self.main_frame, text="Get Password", command=self.get_password, font=("Montserrat Medium", 12), corner_radius=5, width=250, height=34)
+        self.get_password_button.place(x=400, y=380, anchor="center")
+
+        self.how_label = ctk.CTkLabel(self.main_frame, text="To get a password, enter the website name and then click on 'Get Password' button", font=("Montserrat Light", 10))
+        self.how_label.place(x=400, y=480, anchor="center")
 
         self.show_frame(self.register_frame)
 
@@ -97,10 +100,10 @@ class App:
         master_password = self.register_master_password.get()
 
         if username == '' or master_password == '':
-            CTkMessagebox(title="Error", message="Please enter both username and master password")
+            CTkMessagebox(master=self.register_frame,title="Error", message="Please enter both username and master password", font=("Montserrat Medium", 12))
 
         if username == master_password:
-            CTkMessagebox(title="Error", message="Username and master password cannot be the same")
+            CTkMessagebox(master=self.register_frame,title="Error", message="Username and master password cannot be the same", font=("Montserrat Medium", 12))
 
         hashed_master_password = self.hash_password(master_password)
         user_data = {'username': username, 'master_password': hashed_master_password}
@@ -108,12 +111,12 @@ class App:
         if os.path.exists(file_name) and os.path.getsize(file_name) == 0:
             with open(file_name, 'w') as file:
                 json.dump(user_data, file)
-                CTkMessagebox(title="Info", message="Registration complete!!")
+                CTkMessagebox(master=self.register_frame,title="Success", message="Registration complete!!", font=("Montserrat Medium", 12))
                 self.show_frame(self.login_frame)
         else:
             with open(file_name, 'x') as file:
                 json.dump(user_data, file)
-                CTkMessagebox(title="Info", message="Registration complete!!")
+                CTkMessagebox(master=self.register_frame,title="Success", message="Registration complete!!", font=("Montserrat Medium", 12))
                 self.show_frame(self.login_frame)
 
     def login(self):
@@ -126,12 +129,12 @@ class App:
             stored_password_hash = user_data.get('master_password')
             entered_password_hash = self.hash_password(entered_password)
             if entered_password_hash == stored_password_hash and username == user_data.get('username'):
-                CTkMessagebox(title="Success", icon="check", message="Login successful!!")
+                CTkMessagebox(master=self.login_frame, title="Success", icon="check.png", message="Login successful!!", font=("Montserrat Medium", 12), corner_radius=10, icon_size=(40, 40))
                 self.show_frame(self.main_frame)
             else:
-                CTkMessagebox(title="Error", icon="cancel", message="Invalid Login Credentials!")
+                CTkMessagebox(master=self.login_frame,title="Error", icon="cancel", message="Invalid Login Credentials!", font=("Montserrat Medium", 12))
         except Exception:
-            CTkMessagebox(title="Error",icon="cancel", message="You have not registerd. Please Do That!")
+            CTkMessagebox(master=self.login_frame,title="Error",icon="cancel", message="You have not registered. Please Do That!", font=("Montserrat Medium", 12))
 
         key_filename = 'encryption_key.key'
         if os.path.exists(key_filename):
@@ -148,9 +151,9 @@ class App:
         try:
             with open('passwords.json', 'r') as data:
                 view = json.load(data)
-                CTkMessagebox(title="Websites", message='\n'.join(''.join(x['website'].upper()) for x in view))
+                CTkMessagebox(master=self.main_frame,title="Websites", message='\n'.join(''.join(x['website'].upper()) for x in view), font=("Montserrat Medium", 12))
         except FileNotFoundError:
-            CTkMessagebox(title="Error", icon="cancel", message="You have not saved any passwords!")
+            CTkMessagebox(master=self.main_frame,title="Error", icon="cancel", message="You have not saved any passwords!", font=("Montserrat Medium", 12))
 
     def add_password(self):
         website = self.website.get()
@@ -171,7 +174,7 @@ class App:
         with open('passwords.json', 'w') as file:
             json.dump(data, file, indent=4)
 
-        CTkMessagebox(title="Success", icon="check", message="Password Added Successfully!!")
+        CTkMessagebox(master=self.main_frame,title="Success", icon="check", message="Password Added Successfully!!", font=("Montserrat Medium", 12))
 
     def get_password(self):
         website = self.website.get()
@@ -179,7 +182,7 @@ class App:
             return None
 
         if website == '':
-            CTkMessagebox(title="Error", icon="cancel", message="Please enter a website!")
+            CTkMessagebox(master=self.main_frame,title="Error", icon="cancel", message="Please enter a website!", font=("Montserrat Medium", 12))
             return None
 
         try:
@@ -193,12 +196,12 @@ class App:
         for entry in data:
             if entry['website'] == website:
                 decrypted_password = self.decrypt_password(self.cipher, entry['password'])
-                CTkMessagebox(title="Password", message=f"Password For {website} : {decrypted_password}")
+                CTkMessagebox(master=self.main_frame,title="Password", message=f"Password For {website} : {decrypted_password}", font=("Montserrat Medium", 12))
                 password_found = True
                 break
 
         if not password_found:
-            CTkMessagebox(title="Error", icon="cancel", message="Password Not Found!")
+            CTkMessagebox(master=self.main_frame,title="Error", icon="cancel", message="Password Not Found!", font=("Montserrat Medium", 12))
 
 if __name__ == "__main__":
     root = ctk.CTk()
